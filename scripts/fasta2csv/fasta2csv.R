5f0f246cd88801c6d62c53183c6efc2c4ca616ed
mariusrueve <marius.rueve@live.de>
2018-12-04 09:09:06 +0100

    Added comments

--- scripts/fasta2csv/fasta2csv.R
+++ scripts/fasta2csv/fasta2csv.R
@@ -1,14 +1,23 @@
 library(Biostrings)
-transferFastaTo <- function(in_path, out_path, realmiRNA) {
+# Loads the Biosting library into the script
+# Note that the library has to be installed befor running the script
 
+# Defines a function named "fasta2csv"
+fasta2csv <- function(in_path, out_path, realmiRNA) {
 
+    # Uses the function readRNAStringSet from Biostrings on given fasta file (in_path)
     fastaFile <- readRNAStringSet(in_path)
+
+    # Assings the name and the sequence from the fasta file to a variable
     seq_name = names(fastaFile)
     sequence = paste(fastaFile)
 
+    # Creates a data frame
     df <- data.frame(seq_name, sequence, realmiRNA)
     
+    # Writes the data frame to csv file to given path without extra row names
     write.csv(df,out_path,row.names=FALSE)
 }
 
-transferFastaTo("Test.fasta", "Datenbank.csv", 1)
+# Calls the function to test it, has to be swapped out late to be used with snakemake
+fasta2csv("Test.fasta", "Datenbank.csv", 1)
