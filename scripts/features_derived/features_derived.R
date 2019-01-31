f908a549b8eae83f15462f2a3a1d67c627cf96b2
Lukas Jansen <lukas.jansen1997@yahoo.de>
2019-01-31 16:40:28 +0100

    Added some simple feature calcs, Closes #24, closes #23, closes #20, closes #18, closes #9

--- scripts/features_derived/features_derived.R
+++ scripts/features_derived/features_derived.R
@@ -1,3 +1,4 @@
+library(stringr)
 
 features_derived <- function(infile,out){
 
@@ -7,9 +8,17 @@ features_derived <- function(infile,out){
   # Apply the function GC on the matrix of the data, which only contains the sequences  
   length <- apply(as.matrix(csvdf[, "sequence"]),1,nchar)
   NEFE <- csvdf[,"efe"]/length
-
-  total <- cbind(comment=csvdf[,"comment"], length=length, nefe=NEFE)
-
+  dG <-  csvdf[,"mfe"]/length
+  n_stems <- apply(as.matrix(csvdf[,"mfesecstructure"]),1, function(seq){str_count(seq,'(\\(){3,}')})
+  tot_bases <-  apply(as.matrix(csvdf[,"mfesecstructure"]),1, function(seq){str_count(seq,'(\\()')})
+  n_loops <- apply(as.matrix(csvdf[,"mfesecstructure"]),1, function(seq){str_count(seq,'(\\.){3,}')})
+  # GC calc here
+  #mfeI1 <- dG/gccontent
+  mfeI2 <- dG/n_stems
+  mfeI3 <- dG/n_loops
+  mfeI4 <- csvdf[,"mfe"]/tot_bases
+  dP <- tot_bases/length
+  total <- cbind(comment=csvdf[,"comment"], length=length, nefe=NEFE,mfei2=mfeI2,mfei3=mfeI3,mfei4=mfeI4,tot_bases,n_stems,n_loops,dP,dG)
   # Writes Data Frame to a .csv file
   write.csv(total, file=out, row.names=FALSE)
 }
