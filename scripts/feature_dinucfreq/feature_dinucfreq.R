16c3a1845821a32fe8c46e36fe47931ed7a9805f
mariusrueve <marius.rueve@live.de>
2019-02-11 10:25:42 +0100

    Updated feature_dinucfreq.R
    Added lines to get a data frame to be able to merge the results with their sequences.

--- scripts/feature_dinucfreq/feature_dinucfreq.R
+++ scripts/feature_dinucfreq/feature_dinucfreq.R
@@ -7,13 +7,22 @@ feature_dinucfrq <- function(inputcsv){
   
   # Get Sequence Column from DF
   mySequence <- csvdf[, "sequence"]
-  
   print(mySequence)
+  
+  # Output Dataframe
+  countDF <- data.frame(Dinucs = character(),
+                         Count = integer())
+
   
   # For-Loop to iterate over the sequences and apply func count()
   for(i in mySequence){
-    print(count(tolower(s2c(i)), wordsize = 2, alphabet = s2c("acgu")))
+    countDF <- count(tolower(s2c(i)), wordsize = 2, alphabet = s2c("acgu"))
   }
+  # Merge DFs
+  outputDF <- merge(mySequence, countDF)
+  # outputDF <- t(outputDF)
+  outputDF <- outputDF[-c(1)]
+  View(outputDF)
 }
 
 feature_dinucfrq("test.csv")
